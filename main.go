066d0aa93667239b120a8bcd2ca828209f88093c
Snorri Sturluson <snorri.sturluson@ccpgames.com>
2017-10-31 21:33:29 +0000

    Proper connection, parsing json and rendering positions

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"fmt"
 	"net"
 	"encoding/json"
+	"github.com/faiface/pixel/imdraw"
+	"strings"
 )
 
 type Vector3 struct {
@@ -26,8 +28,34 @@ type State struct {
 	Ships [] Ship
 }
 
+type SolarsystemViewer struct {
+	state State
+}
+
+func NewSolarsystemViewer() (*SolarsystemViewer){
+	return &SolarsystemViewer{}
+}
+
+func (viewer* SolarsystemViewer) render(imd* imdraw.IMDraw, atlas* text.Atlas) ([]*text.Text) {
+	labels := []*text.Text{}
+	for i, ship := range(viewer.state.Ships) {
+		x := ship.Position.X
+		y := ship.Position.Y
+		imd.Color = colornames.Black
+		imd.Push(pixel.V(x,y))
+		imd.Circle(10, 1)
+		label := text.New(pixel.V(x, y), atlas)
+		label.Color = colornames.Black
+		fmt.Fprintf( label,"%v", i)
+		labels = append(labels, label)
+	}
+	return labels
+}
+
 func run() {
-	recvBuf := make([]byte, 4096)
+	//x := `{"command": "addship", "owner": 1, "type": 2, "position": {"x": 100.0, "y": 100.0, "z": 0.0}}"`
+	//x := `{"command": "addship", "owner": 1, "type": 2, "position": {"x": 200.0, "y": 100.0, "z": 0.0}}"`
+	//x := `{"command": "addship", "owner": 1, "type": 2, "position": {"x": 200.0, "y": 150.0, "z": 0.0}}"`
 	full_address := "localhost:4041"
 	conn, err := net.Dial("tcp", full_address)
 	if err != nil {
@@ -35,32 +63,10 @@ func run() {
 		return
 	}
 	fmt.Printf("Connection made\n")
-	conn.Write([]byte("{\"command\": \"addship\", \"owner\": 1, \"type\": 2, \"position\": {\"x\": -10.0, \"y\": 10.0, \"z\": 0.0}}"))
-	n, err := conn.Read(recvBuf)
-	if err != nil {
-		fmt.Print(err)
-	}
-	msgReceived := string(recvBuf[:n])
-	fmt.Printf("Message Received: %s\n", msgReceived)
-
-	conn.Write([]byte("{\"command\": \"getstate\"}"))
-	n, err = conn.Read(recvBuf)
-	if err != nil {
-		fmt.Print(err)
-	}
-	msgReceived = string(recvBuf[:n])
-	fmt.Printf("Message Received: %v\n", msgReceived)
-
-	var state State
-	err = json.Unmarshal(recvBuf[:n], &state)
-	if err != nil {
-		fmt.Print(err)
-	}
-	fmt.Printf("%v", state)
 
 	cfg := pixelgl.WindowConfig{
-		Title:  "Pixel Rocks!",
-		Bounds: pixel.R(0, 0, 1024, 768),
+		Title:  "exsim-physics-viewer",
+		Bounds: pixel.R(0, 0, 800, 600),
 		VSync:  true,
 		Resizable: true,
 	}
@@ -69,6 +75,9 @@ func run() {
 		panic(err)
 	}
 
+	viewer := NewSolarsystemViewer()
+	imd := imdraw.New(nil)
+	labels := []*text.Text{}
 
 	atlas := text.NewAtlas(basicfont.Face7x13, text.ASCII, text.RangeTable(unicode.Latin))
 	basicTxt := text.New(pixel.V(10, 100), atlas)
@@ -77,10 +86,38 @@ func run() {
 	fmt.Fprintln(basicTxt, "I support multiple lines!")
 	fmt.Fprintf(basicTxt, "And I'm an %s, yay!", "io.Writer")
 
+	recvChannel := make(chan string)
+	go receive_loop(conn, recvChannel)
 	for !win.Closed() {
+		select {
+		case msgReceived := <- recvChannel:
+			fmt.Printf("Message Received: %s\n", msgReceived)
+			if !strings.HasPrefix(msgReceived, "error:") {
+				json.Unmarshal([]byte(msgReceived), &viewer.state)
+				imd.Clear()
+				labels = viewer.render(imd, atlas)
+			}
+		default:
+			// No data received
+		}
 		win.Clear(colornames.Skyblue)
-		basicTxt.Draw(win, pixel.IM)
+		imd.Draw(win)
+		for _, label := range(labels) {
+			label.Draw(win, pixel.IM)
+		}
 		win.Update()
+	}
+}
+
+func receive_loop(conn net.Conn, c chan string) {
+	recvBuf := make([]byte, 4096)
+	for {
+		n, err := conn.Read(recvBuf)
+		if err != nil {
+			fmt.Print(err)
+		}
+		msgReceived := string(recvBuf[:n])
+		c <- msgReceived
 	}
 }
 
